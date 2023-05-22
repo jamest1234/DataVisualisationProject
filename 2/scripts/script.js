d254d4760e3e18c0cd11b7a7818defb7c3f47d8a
James <103641415@student.swin.edu.au>
2023-05-22 11:49:03 +1000

    a funnel

--- 2/scripts/script.js
+++ 2/scripts/script.js
@@ -1,5 +1,6 @@
 const w = 600;
 const h = 600;
+const padding = 5;
 
 function parseDateString(dateString) {
     var split = dateString.split("/");
@@ -22,6 +23,51 @@ function processCSV(d) {
 
 function createFunnelChart(dataset) {
     console.log(dataset);
+
+    var xScale = d3.scaleLinear()
+        .domain([
+            d3.min(dataset, function(d) {
+                return d.refugees;
+            }),
+            d3.max(dataset, function(d) {
+                return d.refugees;
+            }),
+        ])
+        .range([padding, w-padding]);
+
+    var yScale = d3.scaleBand()
+        .domain(d3.range(dataset.length))
+        .rangeRound([0, h])
+        .paddingInner(0.05);
+
+    var svg = d3.select("#visualisation")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h);
+
+    
+    var colour = d3.scaleLinear()
+        .domain(d3.extent(dataset, function(d) { return d.refugees; }))
+        .range(["#D4FC0A", "#FC870A"])
+        .interpolate(d3.interpolateHcl);
+
+    svg.selectAll("rect")
+        .data(dataset)
+        .enter()
+        .append("rect")
+        .attr("x", function(d, i) {
+            return (w-xScale(d.refugees))/2;
+        })
+        .attr("y", function(d, i) {
+            return yScale(i);
+        })
+        .attr("width", function(d) {
+            return xScale(d.refugees);
+        })
+        .attr("height", yScale.bandwidth())
+        .attr("fill", function(d, i) {
+            return colour(d.refugees);
+        })
 
     // TODO: make funnel chart
 }
